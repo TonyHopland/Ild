24ce460947fe401406141943c186856d585502e1
Tony <tony.hopland@gmail.com>
2026-05-18 07:50:59 +0200

    Cleaning up devcontainer

--- .devcontainer/features/forge-install/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Installing Forge for user $_REMOTE_USER..."
-
-# Run the installer as the primary user
-su - $_REMOTE_USER -c "curl -fsSL https://raw.githubusercontent.com/Robin831/Forge/main/install.sh | bash"
-
-echo "Forge installed successfully"
--- .devcontainer/features/govulncheck/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Installing govulncheck..."
-
-# Install govulncheck using go install
-go install golang.org/x/vuln/cmd/govulncheck@latest
-
-echo "govulncheck installed successfully"
